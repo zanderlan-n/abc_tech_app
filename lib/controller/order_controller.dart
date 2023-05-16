02d1f268e88cfda3dcacf03f859128f714d6dc90
Joao Paulo Pacheco <jppacheco09@hotmail.com>
2023-05-16 00:47:07 -0300

    feat: create oreder controller

---
+++ lib/controller/order_controller.dart
@@ -0,0 +1,26 @@
+import 'package:abc_tech_app/model/assist.dart';
+import 'package:abc_tech_app/service/geolocation_service.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'dart:developer';
+
+class OrderController extends GetxController {
+  final GeolocationServiceInterface _geolocationService;
+  final _formKey = GlobalKey<FormState>();
+  final operatorIdController = TextEditingController();
+  final selectedAssists = <Assist>[].obs;
+
+  OrderController(this._geolocationService);
+
+  @override
+  void onInit() {
+    super.onInit();
+    _geolocationService.start();
+  }
+
+  getLocation() {
+    _geolocationService
+        .getPosition()
+        .then((value) => log(value.toJson().toString()));
+  }
+}
