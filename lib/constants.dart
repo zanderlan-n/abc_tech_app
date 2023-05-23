6d75006c10bd1742a1a3bc009ca1f644bfc3c344
Zanderlan Neto <zanderlan.n@gmail.com>
2023-05-22 21:33:31 -0300

    update api url

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,11 @@
 class Constants {
+  // Prod: Running api on AWS
+  static const url =
+      "http://abctechapi-dev-env.eba-dtyunmps.us-east-2.elasticbeanstalk.com/";
+
   // Dev: Running api locally on Chrome
   // static const url = "http://localhost:8080";
 
   // Dev: Running api locally on Android
-  static const url = "http://192.168.1.118:8080";
+  // static const url = "http://192.168.1.118:8080";
 }
